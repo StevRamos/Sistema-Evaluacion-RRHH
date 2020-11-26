6616236f549bae6f277c07f2abb0bdfff1b5ff41
rodrigo458 <rodrigogalarza458@gmail.com>
2020-11-26 04:40:32 -0500

    colaboradores1.0

--- SistemaEDInterfaces/SistemaEDInterfaces/frmAdmGestColab.cs
+++ SistemaEDInterfaces/SistemaEDInterfaces/frmAdmGestColab.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SistemaEDInterfaces.ColaboradorWS;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,12 +11,36 @@ using System.Windows.Forms;
 
 namespace SistemaEDInterfaces
 {
+
     public partial class frmAdmGestColab : Form
     {
+
+        private ColaboradorWS.ColaboradorWSClient daoColaborador;
+        private PuestoTrabajoWS.PuestoTrabajoWSClient daoPuestoTrabajo;
+        private ColaboradorWS.colaborador colaboradorSeleccionado;
+
+        public ColaboradorWS.colaborador ColaboradorSeleccionado 
+        { get => colaboradorSeleccionado; set => colaboradorSeleccionado = value; }
+
         public frmAdmGestColab()
         {
             InitializeComponent();
+            daoPuestoTrabajo = new PuestoTrabajoWS.PuestoTrabajoWSClient();
+            daoColaborador = new ColaboradorWS.ColaboradorWSClient();
+
+            dgvColaboradores.AutoGenerateColumns = false;
+            //dgvColaboradores.DataSource = new BindingList<ColaboradorWS.colaborador>
+            //    (daoColaborador.listarColaboradores(-1,"").ToArray());
+            dgvColaboradores.DataSource = daoColaborador.listarColaboradores(-1,"");
+
+            cboCargo.DataSource = new BindingList<PuestoTrabajoWS.puestoTrabajo>
+                (daoPuestoTrabajo.listarPuestoTrabajos("").ToArray() );
+            cboCargo.ValueMember = "idPuestoTrabajo";
+            cboCargo.DisplayMember = "nombre";
+            
         }
+
+
 
         private void btnGestColabCargaMav_Click(object sender, EventArgs e)
         {
@@ -25,8 +50,54 @@ namespace SistemaEDInterfaces
 
         private void btnGestColabVerDetalle_Click(object sender, EventArgs e)
         {
-            Form formulario = new frmAdmGestColabVerDetalle();
-            formulario.Show();
+            colaboradorSeleccionado =
+               (ColaboradorWS.colaborador)dgvColaboradores.CurrentRow.DataBoundItem;
+
+            frmAdmGestColabVerDetalle form = new frmAdmGestColabVerDetalle();
+            form.Colaborador = colaboradorSeleccionado;
+            Global.formPrincipal.abrirFormularioHijo(false, form);
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            PuestoTrabajoWS.puestoTrabajo puestoTrabajo;
+            puestoTrabajo = (PuestoTrabajoWS.puestoTrabajo)cboCargo.SelectedItem;
+            
+
+            if (string.IsNullOrEmpty(txtDni.Text))
+            {
+                dgvColaboradores.DataSource = daoColaborador.listarColaboradores(puestoTrabajo.idPuestoTrabajo, "");
+            }
+            else
+            {
+                dgvColaboradores.DataSource = daoColaborador.listarColaboradores(puestoTrabajo.idPuestoTrabajo, txtDni.Text);
+            }
+        }
+
+        private void dgvColaboradores_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            ColaboradorWS.colaborador data = dgvColaboradores.Rows[e.RowIndex].DataBoundItem as ColaboradorWS.colaborador;
+            dgvColaboradores.Rows[e.RowIndex].Cells[2].Value = data.jefe.idColaborador;
+            dgvColaboradores.Rows[e.RowIndex].Cells["Cargo"].Value = data.puestoTrabajo.nombre;
+        }
+
+        private void btnDesactivar_Click(object sender, EventArgs e)
+        {
+            var result = MessageBox.Show("Se eliminaran todos los colaboradores seleccionados",
+                                            "Mensaje de advertencia",
+                                            MessageBoxButtons.OKCancel,
+                                            MessageBoxIcon.Warning);
+            if (result == DialogResult.OK)
+            {
+                foreach (DataGridViewRow row in dgvColaboradores.SelectedRows)
+                {
+
+                    ColaboradorWS.colaborador colaborador = (ColaboradorWS.colaborador)dgvColaboradores.CurrentRow.DataBoundItem;
+                    daoColaborador.eliminarColaborador(colaborador.idColaborador);
+                    dgvColaboradores.DataSource = daoColaborador.listarColaboradores(-1, "");
+                }
+
+            }
         }
     }
 }
--- SistemaEDInterfaces/SistemaEDInterfaces/frmAdmGestColabVerDetalle.Designer.cs
+++ SistemaEDInterfaces/SistemaEDInterfaces/frmAdmGestColabVerDetalle.Designer.cs
@@ -28,7 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtDni = new System.Windows.Forms.TextBox();
             this.btnGestColabDetaRegre = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -39,22 +39,23 @@
             this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.textBox5 = new System.Windows.Forms.TextBox();
-            this.textBox6 = new System.Windows.Forms.TextBox();
-            this.textBox7 = new System.Windows.Forms.TextBox();
-            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.txtApellidos = new System.Windows.Forms.TextBox();
+            this.txtDireccion = new System.Windows.Forms.TextBox();
+            this.txtCargo = new System.Windows.Forms.TextBox();
+            this.txtGerencia = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // textBox1
+            // txtDni
             // 
-            this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(133, 145);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(387, 22);
-            this.textBox1.TabIndex = 54;
+            this.txtDni.Enabled = false;
+            this.txtDni.Location = new System.Drawing.Point(100, 118);
+            this.txtDni.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtDni.Name = "txtDni";
+            this.txtDni.Size = new System.Drawing.Size(291, 20);
+            this.txtDni.TabIndex = 54;
             // 
             // btnGestColabDetaRegre
             // 
@@ -62,10 +63,10 @@
             this.btnGestColabDetaRegre.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnGestColabDetaRegre.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnGestColabDetaRegre.ForeColor = System.Drawing.Color.White;
-            this.btnGestColabDetaRegre.Location = new System.Drawing.Point(487, 491);
-            this.btnGestColabDetaRegre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnGestColabDetaRegre.Location = new System.Drawing.Point(365, 399);
+            this.btnGestColabDetaRegre.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnGestColabDetaRegre.Name = "btnGestColabDetaRegre";
-            this.btnGestColabDetaRegre.Size = new System.Drawing.Size(172, 32);
+            this.btnGestColabDetaRegre.Size = new System.Drawing.Size(129, 26);
             this.btnGestColabDetaRegre.TabIndex = 53;
             this.btnGestColabDetaRegre.Text = "Regresar";
             this.btnGestColabDetaRegre.UseVisualStyleBackColor = false;
@@ -75,9 +76,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(70, 145);
+            this.label1.Location = new System.Drawing.Point(52, 118);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 19);
+            this.label1.Size = new System.Drawing.Size(36, 17);
             this.label1.TabIndex = 52;
             this.label1.Text = "DNI:";
             // 
@@ -86,9 +88,10 @@
             this.label2.Font = new System.Drawing.Font("Century Gothic", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(66)))), ((int)(((byte)(122)))));
             this.label2.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Location = new System.Drawing.Point(9, 7);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(882, 97);
+            this.label2.Size = new System.Drawing.Size(662, 79);
             this.label2.TabIndex = 51;
             this.label2.Text = "Detalles del Colaborador";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
@@ -97,9 +100,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(20, 218);
+            this.label3.Location = new System.Drawing.Point(15, 177);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(94, 19);
+            this.label3.Size = new System.Drawing.Size(77, 17);
             this.label3.TabIndex = 55;
             this.label3.Text = "Nombres :";
             // 
@@ -107,9 +111,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(16, 369);
+            this.label4.Location = new System.Drawing.Point(12, 300);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(98, 19);
+            this.label4.Size = new System.Drawing.Size(82, 17);
             this.label4.TabIndex = 56;
             this.label4.Text = "Dirección :";
             // 
@@ -117,9 +122,10 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(20, 286);
+            this.label5.Location = new System.Drawing.Point(15, 232);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(96, 19);
+            this.label5.Size = new System.Drawing.Size(80, 17);
             this.label5.TabIndex = 57;
             this.label5.Text = "Apellidos :";
             // 
@@ -127,9 +133,10 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(563, 221);
+            this.label6.Location = new System.Drawing.Point(422, 180);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(96, 19);
+            this.label6.Size = new System.Drawing.Size(80, 17);
             this.label6.TabIndex = 58;
             this.label6.Text = "Gerencia :";
             // 
@@ -137,9 +144,10 @@
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(589, 145);
+            this.label7.Location = new System.Drawing.Point(442, 118);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(70, 19);
+            this.label7.Size = new System.Drawing.Size(58, 17);
             this.label7.TabIndex = 59;
             this.label7.Text = "Cargo :";
             // 
@@ -147,9 +155,10 @@
             // 
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(570, 369);
+            this.label8.Location = new System.Drawing.Point(428, 300);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(89, 19);
+            this.label8.Size = new System.Drawing.Size(74, 17);
             this.label8.TabIndex = 60;
             this.label8.Text = "Teléfono :";
             // 
@@ -157,81 +166,89 @@
             // 
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Century Gothic", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(584, 286);
+            this.label9.Location = new System.Drawing.Point(438, 232);
+            this.label9.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(75, 19);
+            this.label9.Size = new System.Drawing.Size(62, 17);
             this.label9.TabIndex = 61;
             this.label9.Text = "Correo :";
             // 
-            // textBox2
+            // txtNombres
             // 
-            this.textBox2.Enabled = false;
-            this.textBox2.Location = new System.Drawing.Point(133, 218);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(387, 22);
-            this.textBox2.TabIndex = 62;
+            this.txtNombres.Enabled = false;
+            this.txtNombres.Location = new System.Drawing.Point(100, 177);
+            this.txtNombres.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(291, 20);
+            this.txtNombres.TabIndex = 62;
             // 
-            // textBox3
+            // txtApellidos
             // 
-            this.textBox3.Enabled = false;
-            this.textBox3.Location = new System.Drawing.Point(133, 286);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(387, 22);
-            this.textBox3.TabIndex = 63;
+            this.txtApellidos.Enabled = false;
+            this.txtApellidos.Location = new System.Drawing.Point(100, 232);
+            this.txtApellidos.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtApellidos.Name = "txtApellidos";
+            this.txtApellidos.Size = new System.Drawing.Size(291, 20);
+            this.txtApellidos.TabIndex = 63;
             // 
-            // textBox4
+            // txtDireccion
             // 
-            this.textBox4.Enabled = false;
-            this.textBox4.Location = new System.Drawing.Point(133, 369);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(387, 22);
-            this.textBox4.TabIndex = 64;
+            this.txtDireccion.Enabled = false;
+            this.txtDireccion.Location = new System.Drawing.Point(100, 300);
+            this.txtDireccion.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtDireccion.Name = "txtDireccion";
+            this.txtDireccion.Size = new System.Drawing.Size(291, 20);
+            this.txtDireccion.TabIndex = 64;
             // 
-            // textBox5
+            // txtCargo
             // 
-            this.textBox5.Enabled = false;
-            this.textBox5.Location = new System.Drawing.Point(665, 145);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(387, 22);
-            this.textBox5.TabIndex = 65;
+            this.txtCargo.Enabled = false;
+            this.txtCargo.Location = new System.Drawing.Point(499, 118);
+            this.txtCargo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtCargo.Name = "txtCargo";
+            this.txtCargo.Size = new System.Drawing.Size(291, 20);
+            this.txtCargo.TabIndex = 65;
             // 
-            // textBox6
+            // txtGerencia
             // 
-            this.textBox6.Enabled = false;
-            this.textBox6.Location = new System.Drawing.Point(665, 218);
-            this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(387, 22);
-            this.textBox6.TabIndex = 66;
+            this.txtGerencia.Enabled = false;
+            this.txtGerencia.Location = new System.Drawing.Point(499, 177);
+            this.txtGerencia.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtGerencia.Name = "txtGerencia";
+            this.txtGerencia.Size = new System.Drawing.Size(291, 20);
+            this.txtGerencia.TabIndex = 66;
             // 
-            // textBox7
+            // txtCorreo
             // 
-            this.textBox7.Enabled = false;
-            this.textBox7.Location = new System.Drawing.Point(665, 286);
-            this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(387, 22);
-            this.textBox7.TabIndex = 67;
+            this.txtCorreo.Enabled = false;
+            this.txtCorreo.Location = new System.Drawing.Point(499, 232);
+            this.txtCorreo.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(291, 20);
+            this.txtCorreo.TabIndex = 67;
             // 
-            // textBox8
+            // txtTelefono
             // 
-            this.textBox8.Enabled = false;
-            this.textBox8.Location = new System.Drawing.Point(665, 369);
-            this.textBox8.Name = "textBox8";
-            this.textBox8.Size = new System.Drawing.Size(387, 22);
-            this.textBox8.TabIndex = 68;
+            this.txtTelefono.Enabled = false;
+            this.txtTelefono.Location = new System.Drawing.Point(499, 300);
+            this.txtTelefono.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(291, 20);
+            this.txtTelefono.TabIndex = 68;
             // 
             // frmAdmGestColabVerDetalle
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(1149, 586);
-            this.Controls.Add(this.textBox8);
-            this.Controls.Add(this.textBox7);
-            this.Controls.Add(this.textBox6);
-            this.Controls.Add(this.textBox5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
+            this.ClientSize = new System.Drawing.Size(862, 476);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.txtGerencia);
+            this.Controls.Add(this.txtCargo);
+            this.Controls.Add(this.txtDireccion);
+            this.Controls.Add(this.txtApellidos);
+            this.Controls.Add(this.txtNombres);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.label7);
@@ -239,12 +256,14 @@
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtDni);
             this.Controls.Add(this.btnGestColabDetaRegre);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "frmAdmGestColabVerDetalle";
             this.Text = "frmAdmGestColabVerDetalle";
+            this.Load += new System.EventHandler(this.frmAdmGestColabVerDetalle_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -252,7 +271,7 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtDni;
         private System.Windows.Forms.Button btnGestColabDetaRegre;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
@@ -263,12 +282,12 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label8;
         private System.Windows.Forms.Label label9;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.TextBox textBox5;
-        private System.Windows.Forms.TextBox textBox6;
-        private System.Windows.Forms.TextBox textBox7;
-        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.TextBox txtApellidos;
+        private System.Windows.Forms.TextBox txtDireccion;
+        private System.Windows.Forms.TextBox txtCargo;
+        private System.Windows.Forms.TextBox txtGerencia;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.TextBox txtTelefono;
     }
 }
